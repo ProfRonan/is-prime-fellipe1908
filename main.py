484bbb11b02e109ca189de54a6a61dc3f9e3247a
fellipe1908 <128155054+fellipe1908@users.noreply.github.com>
2023-04-13 17:30:36 +0000

    Exercicio concluido

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 print("Digite um número: ")
-a = int(input(">>>")) + 1
-b = list(range(2,a))
-b.pop()
-print(b)
-if a % b == 0:
-    print("Numero Não é primo")
+a = int(input(">>>"))
+if a < 1:
+    print("Número inválido")
 else:
-    print("é um número primo")
+
+    if a == 2 or  a==3 or a==5 or a==7:
+        print("Primo")
+    elif a % 2 == 0 or a % 3 == 0 or a % 5 == 0 or a % 7 == 0 or a == 1:
+        print("Não primo")
+    else:
+        print("Primo")
